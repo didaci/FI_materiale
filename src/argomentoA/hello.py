4a6814646b5d4bef56e2d706f045702d52da8f6b
didaci <didaci@unica.it>
2026-02-28 19:44:52 +0100

    Initial commit

---
+++ src/argomentoA/hello.py
@@ -0,0 +1,30 @@
+print("HELLO 1!")
+
+print("HELLO!")
+
+print("HELLO!")
+
+print("HELLO!")
+
+print("HELLO 5!")
+
+a= 7
+
+def f():
+	b=10
+	print(10)
+
+print("HELLO!")
+
+print("HELLO!")
+
+print("HELLO!")
+
+print("HELLO!")
+
+print("HELLO 10!")
+
+print("HELLO!")
+
+print("HELLO!")
+
